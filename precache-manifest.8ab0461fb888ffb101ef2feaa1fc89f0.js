7165d58c2c7736a11fd90b98388eccd7cbce8c4c
pawansa3 <pawansa3@noreply.github.com>
2019-05-03 00:47:20 +0530

    Updates

---
+++ precache-manifest.8ab0461fb888ffb101ef2feaa1fc89f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0114513e40a91140dfd499fae11a615",
+    "url": "/online_pizza_app/static/media/body-bg.c0114513.jpg"
+  },
+  {
+    "revision": "da7458e6c88f1d362a99",
+    "url": "/online_pizza_app/static/js/runtime~main.d0c7cd42.js"
+  },
+  {
+    "revision": "5118dd759c68f1bbdea7",
+    "url": "/online_pizza_app/static/js/main.43c4f425.chunk.js"
+  },
+  {
+    "revision": "cef9c2f1f66592b29063",
+    "url": "/online_pizza_app/static/js/2.2230911d.chunk.js"
+  },
+  {
+    "revision": "5118dd759c68f1bbdea7",
+    "url": "/online_pizza_app/static/css/main.2285c8d3.chunk.css"
+  },
+  {
+    "revision": "cef9c2f1f66592b29063",
+    "url": "/online_pizza_app/static/css/2.1705c23f.chunk.css"
+  },
+  {
+    "revision": "282b01f73845cdf21b00a4f7bc7c9188",
+    "url": "/online_pizza_app/index.html"
+  }
+];
